b8319e5041c2251ecdb811404a7fe0123dcf7136
Juan Diego Ordoñez <juandiego99222@gmail.com>
2021-12-23 08:36:44 -0500

    aa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,16 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
+  },
+  
+  
 ];
 
 @NgModule({
